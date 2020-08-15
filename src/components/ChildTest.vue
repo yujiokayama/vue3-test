406eab715febe24d42af23aa420ef27178a96afa
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-08-16 00:47:20 +0900

    v-model複数できる

---
+++ src/components/ChildTest.vue
@@ -0,0 +1,28 @@
+<template>
+  {{ name }}
+  {{ age }}
+  <form>
+    <label>name</label>
+    <input type="text" @input="$emit('update:name', $event.target.value)" />
+    <label>message</label>
+    <input type="text" @input="$emit('update:age', $event.target.value)" />
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: " ChildTest",
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    age: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
--- src/views/CompositionTest.vue
+++ src/views/CompositionTest.vue
@@ -4,33 +4,84 @@
     <h2>リアクティブなプロパティの宣言 ref, reactive</h2>
     <p>reactive: {{ state.messageOne }}</p>
     <p>ref: {{ messageTwo }}</p>
+    <h2>computed</h2>
+    {{ helloVue }}
+    {{ helloMessage("hoge") }}
+
+    <h2>v-modelの仕様変更(一つの要素に複数のプロパティを指定できる)</h2>
+    <ChildTest v-model:name="state.name" v-model:age="state.age" />
+
+    <h2>v-for</h2>
+    <ul>
+      <li v-for="member in state.memberList" :key="member.id">
+        {{ member.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from "vue";
-
-type HogeType<T, S> = {
-  name: T;
-  age: S;
-};
-
-const hogeTypeObj: HogeType<string, number> = {
-  name: "hoge",
-  age: 50,
-};
+import { defineComponent, computed, reactive, ref, onMounted } from "vue";
+import ChildTest from "../components/ChildTest.vue";
 
 export default defineComponent({
   name: " CompositionTest",
+  components: {
+    ChildTest,
+  },
   setup() {
-    const state = reactive<{ messageOne: string }>({
-      messageOne: "Hello",
+    type Member = {
+      id: number;
+      name: string;
+    };
+
+    /**
+     * reactive data
+     */
+    const state = reactive<{
+      name: string;
+      age: string;
+      memberList: Member[];
+      message: string;
+    }>({
+      name: "デフォルト",
+      age: "",
+      memberList: [
+        {
+          id: 1,
+          name: "ポンタ",
+        },
+        {
+          id: 2,
+          name: "たぬきち",
+        },
+      ],
+      message: "こんにちは",
     });
-    const messageTwo = ref<string>("こんにちは");
+    const messageHello = ref<string>("こんにちは");
+
+    /**
+     * computed
+     */
+    const helloVue = computed(() => {
+      return `${state.message} Vue3!!`;
+    });
+    const helloMessage = computed(() => (message: string) =>
+      `こんにちは${message}!`
+    );
+
+    /**
+     * lifecycle hook
+     */
+    onMounted(() => {
+      console.log("is mounted");
+    });
 
     return {
       state,
-      messageTwo,
+      messageHello,
+      helloVue,
+      helloMessage,
     };
   },
 });
