bd954f4dc2f4b15eda1119d37fac1e47b256a7bb
yuji-okayama <y-okayama@tribeck.jp>
2020-09-03 22:49:04 +0900

    vuex getters

--- src/views/VuexTest.vue
+++ src/views/VuexTest.vue
@@ -1,26 +1,42 @@
 <template>
   <h1>Vuexのテスト</h1>
-  {{ count.counter }}
-  <button @click="setCount(10)">count up !! </button>
+  {{ state.counter }}
+  2倍: {{ countDouble }}
+  <button @click="setCount(10)">count up !!</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'VuexTest',
   setup() {
+    const store = useStore()
 
-    const store = useStore();
-    const count = ref(store.state);
+    /**
+     * state
+     */
+    const state = ref(store.state)
+
+    /**
+     * actions
+     */
     const setCount = (count: number) => {
-      store.dispatch("setCount", count);
-    };
+      store.dispatch('setCount', count)
+    }
+
+    /**
+     * getters
+     */
+    const countDouble = computed(() => {
+      return store.getters.countDouble
+    })
 
     return {
-      count,
+      state,
       setCount,
+      countDouble
     }
   }
 })
